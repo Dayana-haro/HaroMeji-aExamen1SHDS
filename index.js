db92e5775dba52eae52029a1b89fafb95b1a213e
Dayana-Haro <harocarolina24@gmail.com>
2024-12-09 08:52:19 -0500

    Servidor

---
+++ index.js
@@ -0,0 +1,29 @@
+// Protocolo de intercambio
+var http = require('http');
+
+// Crear servidor
+var server = http.createServer();
+
+// Servicio web
+function haromejia(req, resp) {
+    // Estado satisfactorio 200, mensaje tipo texto plano configurado por JSON
+    resp.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    // Escribo el mensaje
+    resp.write('Dayana Haro Mejía\n');
+    resp.write('Edad: 20 años\n');
+    resp.write('Estudiante de la carrera de Desarrollo de Software\n');
+    resp.write('Hobbies:\n');
+    resp.write('- Leer\n');
+    resp.write('- Hacer deporte\n');
+    resp.write('- Mirar series relacionadas con romance y criminología\n');
+
+    // Finalizar la comunicación entre cliente y servidor
+    resp.end();
+}
+
+server.on('request', haromejia);
+
+server.listen(3001, function () {
+    console.log("La aplicación está corriendo en el puerto 3001");
+});
